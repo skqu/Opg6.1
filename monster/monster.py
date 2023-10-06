f7d6efbf1e6e8ae4bcb912ca5eb55ba582e67370
Stefan Quvang <skquc@iba.dk>
2023-10-06 14:04:42 +0200

    Added new method and comply to pep-8

--- monster/monster.py
+++ monster/monster.py
@@ -1,4 +1,4 @@
-class monster:
+class Monster:
     """ 
     Name: Monster
     Description: 
@@ -11,7 +11,7 @@ class monster:
     """
 
     
-    def __init__(self) -> None:
+    def __init__(self, health, defence, strength, steps) -> None:
         """
         name: __init__
         description: The constructor of the monster class. 
@@ -22,18 +22,35 @@ class monster:
         param: steps: The amount of steps the monster can take each turn. 
         """
 
-    def monsterAttack() -> int:
+
+    def monster_attack(self) -> int:
         """
-        name: monsterAttack
+        name: monster_attack
         description: The method handles the attack of the monster. 
         return: The amount of attack the monster perform.
         """
 
-    def monsterMove(pos) -> list:
+
+    def monster_move(self, pos) -> list:
         """
-        name: monsterMove
+        name: monster_move
         description: take det monsters position, and add the movement to that position. 
         Make a check to see if the ammount of steps is allowed. 
         return: self.pos: the new position of the monster. 
         """
 
+    def is_alive() -> bool:
+        """
+        name: is_alive
+        description: Check if the hero is still alive. 
+        param: none:
+        return: self.alive: return true or false, wether or not it is alive. 
+        """
+
+    def get_stat() -> dict:
+        """
+        name: get_stat()
+        description: return the stat of the monster. 
+        param: none:
+        return: stat: dictionary with the stats. 
+        """
